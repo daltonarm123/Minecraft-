b628767335e159c85abb2b8804b35412fa91f350
dalton <146393850+daltonarm123@users.noreply.github.com>
2026-07-12 22:02:13 -0500

    Add portal use status values

---
+++ server-core/src/main/java/com/community/servercore/service/PortalUseStatus.java
@@ -0,0 +1,11 @@
+package com.community.servercore.service;
+
+public enum PortalUseStatus {
+    SUCCESS,
+    NO_PORTAL,
+    DISABLED,
+    ACCESS_DENIED,
+    COOLDOWN_ACTIVE,
+    TELEPORT_IN_PROGRESS,
+    TELEPORT_FAILED
+}
