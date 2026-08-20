79c2e9061603ba665de97c99e31108ce2ea47667
dalton <146393850+daltonarm123@users.noreply.github.com>
2026-08-19 19:19:03 -0500

    Register ServerCore shop with matching screen type

--- servercore-neoforge/src/main/java/com/community/servercore/neoforge/NeoForgeClientEvents.java
+++ servercore-neoforge/src/main/java/com/community/servercore/neoforge/NeoForgeClientEvents.java
@@ -1,6 +1,5 @@
 package com.community.servercore.neoforge;
 
-import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -10,9 +9,8 @@ import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 @EventBusSubscriber(modid = ServerCoreMod.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 final class NeoForgeClientEvents {
     @SubscribeEvent
-    @SuppressWarnings({"unchecked", "rawtypes"})
     static void onRegisterMenuScreens(RegisterMenuScreensEvent event) {
-        event.register((net.minecraft.world.inventory.MenuType) ServerCoreMod.SHOP_MENU.get(), ContainerScreen::new);
+        event.register(ServerCoreMod.SHOP_MENU.get(), NeoForgeShopScreen::new);
     }
 
     private NeoForgeClientEvents() {}
