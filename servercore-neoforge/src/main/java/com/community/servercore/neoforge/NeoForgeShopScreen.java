728def8b0821e8de098baa7e0cac82cc23b4461b
dalton <146393850+daltonarm123@users.noreply.github.com>
2026-08-19 19:29:42 -0500

    Fix shop screen renderBackground signature

--- servercore-neoforge/src/main/java/com/community/servercore/neoforge/NeoForgeShopScreen.java
+++ servercore-neoforge/src/main/java/com/community/servercore/neoforge/NeoForgeShopScreen.java
@@ -19,7 +19,7 @@ public final class NeoForgeShopScreen extends AbstractContainerScreen<NeoForgeSh
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(graphics);
+        this.renderBackground(graphics, mouseX, mouseY, partialTick);
         super.render(graphics, mouseX, mouseY, partialTick);
         this.renderTooltip(graphics, mouseX, mouseY);
     }
