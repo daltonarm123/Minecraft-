dc121b4b8eb818e80d4327fcb1d6eeeb429eae43
dalton <146393850+daltonarm123@users.noreply.github.com>
2026-08-19 19:18:58 -0500

    Add typed ServerCore shop screen

---
+++ servercore-neoforge/src/main/java/com/community/servercore/neoforge/NeoForgeShopScreen.java
@@ -0,0 +1,31 @@
+package com.community.servercore.neoforge;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+
+/** Client screen for the ServerCore shop menu. */
+public final class NeoForgeShopScreen extends AbstractContainerScreen<NeoForgeShopMenu> {
+    private static final ResourceLocation BACKGROUND =
+            ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/container/generic_54.png");
+
+    public NeoForgeShopScreen(NeoForgeShopMenu menu, Inventory playerInventory, Component title) {
+        super(menu, playerInventory, title);
+        this.imageHeight = 222;
+        this.inventoryLabelY = this.imageHeight - 94;
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        this.renderBackground(graphics);
+        super.render(graphics, mouseX, mouseY, partialTick);
+        this.renderTooltip(graphics, mouseX, mouseY);
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
+        graphics.blit(BACKGROUND, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
+    }
+}
